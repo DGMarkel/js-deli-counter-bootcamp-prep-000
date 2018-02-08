5fcf3229d0bd6b621e22b97f295f971c52977b4f
David Markel <davidgmarkel@gmail.com>
2018-02-08 16:42:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(katzDeli, name) {
   katzDeli.push(name);
-  return `Welcome, ${name}. You are number ${katzDeli.indexOf(name + 1)} in line.`;
+  return `Welcome, ${name}. You are number ${katzDeli.indexOf(name) + 1} in line.`;
 }
 
 
