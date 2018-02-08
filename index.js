ec7456aabd6a0e4fe70f7bc8f5431e4f6db4d429
David Markel <davidgmarkel@gmail.com>
2018-02-08 16:13:18 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+
+function takeANumber(katzDeli, name) {
+  katzDeli.push(name);
+  return `Welcome, ${name}. You are number ${katzDeli.indexOf(name) + 1} in line.`
+}
+
+function nowServing(deliLine, name) {
+  if (deliLine[0] === undefined) {
+    return 'There is nobody waiting to be served!';
+  } else {
+      return `Currently serving ${deliLine.shift(0)}.`;
+  }
+}
+
+function currentLine(deliLine) {
+  if (deliLine[0] === undefined) {
+    return 'The line is currently empty.';
+  } else {
+    for (let i = 1; i < deliLine.length; i++) {
+      deliLine.unshift('The line is currently: ');
+      deliLine[i] = (i) + '. ' + deliLine[i];
+    }
+    return deliLine.toString();
+  }
+}
